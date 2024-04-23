bae4dc011b2bc0b61315ddd2de3f20ea13b91f5e
Disruptor <davidsantiagojimenez16@gmail.com>
2024-04-22 19:22:50 -0500

    "Se quita el form para captar datos y se pone un array ya lleno de objetos para usar el ngFor "

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,36 +7,50 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  arrCells:Celular[] = [];
-
-  formData:FormGroup;
+  arrCells: Celular[] = [
+    {
+        marca: "Samsung",
+        modelo: "Galaxy S21",
+        precio: 999,
+        almacenamientoInterno: 128,
+        color: "Negro",
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_701848-MLA53734668228_022023-O.webp"
+    },
+    {
+        marca: "Apple",
+        modelo: "iPhone 13",
+        precio: 1099,
+        almacenamientoInterno: 256,
+        color: "Plata",
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_821416-MLA47781962896_102021-O.webp"
+    },
+    {
+        marca: "Xiaomi",
+        modelo: "Mi 11",
+        precio: 799,
+        almacenamientoInterno: 256,
+        color: "Azul",
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_644644-MLU69665123481_052023-O.webp"
+    },
+    {
+        marca: "OnePlus",
+        modelo: "9 Pro",
+        precio: 899,
+        almacenamientoInterno: 256,
+        color: "Verde",
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_864695-MLA47698970218_092021-O.webp"
+    },
+    {
+        marca: "Google",
+        modelo: "Pixel 6",
+        precio: 699,
+        almacenamientoInterno: 128,
+        color: "Blanco",
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_636688-MLA74650240550_022024-O.webp"
+    }
+];
 
   ngOnInit(){
-    this.formData = new FormGroup({
-      marca: new FormControl(),
-      modelo: new FormControl(),
-      precio: new FormControl(),
-      almacenamiento: new FormControl()
-    })
   }
-
-
-  agregarTelefono(){
-      let celular: Celular = {
-        marca: this.formData.get('marca')?.value,
-        modelo: this.formData.get('modelo')?.value,
-        precio: this.formData.get('precio')?.value,
-        almacenamientoInterno: this.formData.get('almacenamiento')?.value
-      };
-
-      this.arrCells.push(celular);
-      this.formData.reset();
-  }
-
-  mostrarArrays(){
-    console.log(this.arrCells);
-  }
-
-  
 
 }
--- src/app/home/producto.interface.ts
+++ src/app/home/producto.interface.ts
@@ -1,6 +1,10 @@
+// se exporta la interfaz del celular con sus respectivos atributos
+
 export interface Celular {
     marca: string;
     modelo: string;
     precio: number;
     almacenamientoInterno: number;
-  }
+    color:string;
+    imagen:string;
+  } 
